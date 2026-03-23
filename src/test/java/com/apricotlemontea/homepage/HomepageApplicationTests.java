5866c53bdef781ad5357c99dff9ef433b5fa2605
杏仁レモンティー <apricotlemontea@gmail.com>
2026-03-23 21:33:55 +0900

    init

---
+++ src/test/java/com/apricotlemontea/homepage/HomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apricotlemontea.homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
